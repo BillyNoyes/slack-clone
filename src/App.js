85a0696f9c07c097fbe0ba02d3ea1e31cc61dfe1
William (Billy) Noyes <wbillypn@gmail.com>
2020-08-12 16:38:08 +0100

    Used Sidebar in App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
     <div className="app">
       <Header />
-      {/* Sidebar */}
-      {/* React-Router */}
+      <div className="app__body">
+        <Sidebar />
+        {/* React-Router */}
+      </div>
     </div>
   );
 }
