26788732950a87222772c607bf692e0daa07f4a8
William (Billy) Noyes <wbillypn@gmail.com>
2020-08-12 17:10:06 +0100

    Imported useEffect

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Sidebar.css";
 import SidebarOption from "./SidebarOption";
 import db from "./firebase";
