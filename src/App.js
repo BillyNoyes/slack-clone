dbe046a359d582df79b935ceb42f7c162c1e6abd
William (Billy) Noyes <wbillypn@gmail.com>
2020-08-12 17:24:51 +0100

    Created Router to allow for switching between channels

--- src/App.js
+++ src/App.js
@@ -2,15 +2,27 @@ import React from "react";
 import "./App.css";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+import Chat from "./Chat";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <div className="app__body">
-        <Sidebar />
-        {/* React-Router */}
-      </div>
+      <Router>
+        <Header />
+        <div className="app__body">
+          <Sidebar />
+
+          <Switch>
+            <Route path="/room/:roomId">
+              <Chat />
+            </Route>
+            <Route path="/">
+              <h1>Welcome</h1>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
