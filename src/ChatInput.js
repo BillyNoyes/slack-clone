a1d96389393a2ddd9f173a1ff28be48c4aa95404
William (Billy) Noyes <wbillypn@gmail.com>
2020-08-13 14:58:25 +0100

    Created ChatInput component

---
+++ src/ChatInput.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./ChatInput.css";
+
+function ChatInput() {
+  return (
+    <div className="chatInput">
+      <h1></h1>
+    </div>
+  );
+}
+
+export default ChatInput;
